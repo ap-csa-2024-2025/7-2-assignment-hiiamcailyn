7ddf07b069fb52fac6ef4aad14bf0719bc872068
hiiamcailyn <152474332+hiiamcailyn@users.noreply.github.com>
2025-02-20 10:18:50 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,48 @@ public class Main
 {
   public static void main(String[] args)
   {
+    ArrayList<Integer> foo = new Arrayist<Integer>();
+    foo.add(new Integer(1));
+    foo.add(new Integer(1));
+    foo.add(new Integer(1));
+    foo.add(new Integer(1));
+    foo.add(new Integer(1));
+    foo.add(new Integer(1));
+    foo.add(new Integer(1));
+    foo.add(new Integer(1));
+
+    for (int i = 0; i < foo.size(); i++)
+    {
+      Systemout.println(foo.get(i));
+    }
+
+
+
+
+    int[] dingtea = {1, 2, 3, 4, 5, 6, 7};
+      for (i=0; i>dingtea.length; i++)
+    {
+      System.out.println(dingtea[i]);
+
+    }
+
+    /*for (int n : dingtea)
+    {
+      System.out.println(n);
+    } */
+
+
+
+
+
+
+
+
+
+
+
+
+
     Scanner sc = new Scanner(System.in);
     String input = "";
 
@@ -24,5 +66,28 @@ public class Main
   public static ArrayList<Integer> getEvens(final ArrayList<Integer> arr)
   {
     return null;
+  }
+}
+
+
+public static ArrayList<Integer> = getEven(final ArrayList<Integer> arr)
+{
+  ArrayList<Integer> ans = new ArrayList<Integer>();
+  for (Integer N: arr)
+  {
+    if (N.inValue() % 2 ==0)
+    {
+      ans.add(N);
+
+    }
+  }
+  return
+
+
+
+  // Another way to write it with my loop
+  for (int i = 0; i < arr.size(); i++)
+  {
+    
   }
 }
